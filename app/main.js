17de7c8edc54676430eb3f32d35f788167d3cc88
Yunlong Jiao <1689630415@qq.com>
2016-09-28 12:19:10 +0800

    添加注释

--- app/main.js
+++ app/main.js
@@ -4,12 +4,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Test from './components/Test'
-import { AppContainer } from 'react-hot-loader';
+import { AppContainer } from 'react-hot-loader';//新版本需要 AppContainer 包裹整个App 以便热替。
 ReactDOM.render(<AppContainer>
         <Test/>
     </AppContainer>,document.getElementById('test'));
 // Hot Module Replacement API
-if (module.hot) {
+if (module.hot) {//react-hot-loader 暴露了module，所以需要添加如下代码进行判断。
     module.hot.accept('./components/Test', () => {
         const Test = require('./components/Test').default;
         ReactDOM.render(
